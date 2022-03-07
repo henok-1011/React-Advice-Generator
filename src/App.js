25bef0fc37e230bc227402b36b5c1bb1fb682715
henok-1011 <64855405+henok-1011@users.noreply.github.com>
2022-03-06 19:39:51 -1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import axios from "axios";
+import {useState,useEffect} from "react"
+
+export default function App() {
+  const [advice,setAdvice] = useState(null);
+  const fetchAdvice=()=>{
+    axios.get("https://api.adviceslip.com/advice").then((response)=>{setAdvice(response.data);console.log(advice)});
+  }  
+  useEffect(()=>{
+    fetchAdvice();
+  },[])
+  
+  
+  return (
+    <div className="advice__container">
+      <h5 className="advice__id">Advice #{advice?.slip?.id}</h5>
+      <h2 className="advice">"{advice?.slip?.advice}"</h2> 
+      <svg className="adivce__separator__moblie" width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+      <svg className="adivce__separator__desk" width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+      <svg onClick={fetchAdvice} className="button" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+    </div>
+  );
+}
